91ae38c35b77f6e3305b0ae16094e726d45d1100
HuongThao <nguyenhuongthao220296@gmail.com>
2020-04-20 17:18:24 +0900

    Improve cat eat animal

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ var Dog = require('./Dog');
 var Cat = require('./Cat');
 var Mouse = require('./Mouse');
 
-
 var dog = new Dog('Key');
-var tom =  new Cat('Meo');
 
 
-var mouse = new Mouse();
 
-dog.eat(tom);
-console.log(dog);
+var cat = new Cat('tom');
+var mouse = new Mouse('Mikey','yellow');
+try {
+	cat.eat(dog);
+} catch (error) {
+	console.log('Error while catch eating a dog');
+
+}
+
+cat.eat(dog);
+console.log(cat);
 
